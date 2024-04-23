d2109f0cbc8efdb7e606ba6d4a964705d52faaca
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-23 23:58:50 +0200

    feat: add light calculation on the 3D cards

--- src/app/three-d-card/three-d-card.component.ts
+++ src/app/three-d-card/three-d-card.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-three-d-card',
+  selector: "app-three-d-card",
   standalone: true,
   imports: [],
-  templateUrl: './three-d-card.component.html',
-  styleUrl: './three-d-card.component.css'
+  templateUrl: "./three-d-card.component.html",
+  styleUrl: "./three-d-card.component.css",
 })
 export class ThreeDCardComponent {
+  
+  changePerspective(event: any) {
+    const light = event.target.querySelector(".light") as HTMLElement;
+    const card = (event.target as HTMLElement);
+
+    const lightAngle = this.calculateLightAngle(event.layerX, event.layerY, card);
+
+    light.style.transform = `rotate(${-lightAngle}deg)`;
+  }
+
+  calculateLightAngle(curX: number, curY: number, card: HTMLElement) {
+    const xFromCenter = curX - card.offsetWidth / 2;
+    const yFromCenter = card.offsetHeight / 2 - curY ;
+    return Math.atan(yFromCenter / xFromCenter) * (180 / Math.PI);
+  }
 
 }
