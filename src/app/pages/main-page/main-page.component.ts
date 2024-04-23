596ac026d0c358e7b74a2b621a5529f1d00272bf
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-23 22:04:49 +0200

    feat: add the scroll-button in the about section

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
+import { ScrollButtonComponent } from '../../scroll-button/scroll-button.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ScrollButtonComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
