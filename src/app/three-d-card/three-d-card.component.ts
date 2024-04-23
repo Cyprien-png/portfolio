d27f25367cb0de3724b936880d0140a1811b7cb5
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-24 00:19:18 +0200

    feat: add card 3D rotation

--- src/app/three-d-card/three-d-card.component.ts
+++ src/app/three-d-card/three-d-card.component.ts
@@ -8,20 +8,24 @@ import { Component } from "@angular/core";
   styleUrl: "./three-d-card.component.css",
 })
 export class ThreeDCardComponent {
-  
+
   changePerspective(event: any) {
     const light = event.target.querySelector(".light") as HTMLElement;
     const card = (event.target as HTMLElement);
+    const xFromCenter = event.layerX - card.offsetWidth / 2;
+    const yFromCenter = card.offsetHeight / 2 - event.layerY ;
 
-    const lightAngle = this.calculateLightAngle(event.layerX, event.layerY, card);
+    const lightAngle = this.calculateLightAngle(xFromCenter, yFromCenter);
 
-    light.style.transform = `rotate(${-lightAngle}deg)`;
+    light.style.transform = `translate(${-xFromCenter}px, ${yFromCenter}px) rotate(${-lightAngle}deg)`;
+    card.style.transform = `rotateX(${this.calculateCardAngle(card.offsetHeight, yFromCenter)}deg)  rotateY(${this.calculateCardAngle(card.offsetWidth, xFromCenter)}deg)`;
   }
 
-  calculateLightAngle(curX: number, curY: number, card: HTMLElement) {
-    const xFromCenter = curX - card.offsetWidth / 2;
-    const yFromCenter = card.offsetHeight / 2 - curY ;
+  calculateLightAngle(xFromCenter: number, yFromCenter: number) {
     return Math.atan(yFromCenter / xFromCenter) * (180 / Math.PI);
   }
 
+  calculateCardAngle(cardLenght: number, curPosition: number) {
+    return (curPosition / cardLenght * 14) 
+  }
 }
