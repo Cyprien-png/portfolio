80b5d70b882581109e987cadc71d0024b9def273
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-24 00:20:13 +0200

    feat: add three card templates in the projects section

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { ScrollButtonComponent } from '../../scroll-button/scroll-button.component';
+import { ThreeDCardComponent } from '../../three-d-card/three-d-card.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavbarComponent, ScrollButtonComponent],
+  imports: [NavbarComponent, ScrollButtonComponent, ThreeDCardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
