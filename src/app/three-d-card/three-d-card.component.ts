43ae33817c6734262484d3f073700d04568928c5
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-23 23:04:12 +0200

    feat: add basie layout of 3D card component

---
+++ src/app/three-d-card/three-d-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-d-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './three-d-card.component.html',
+  styleUrl: './three-d-card.component.css'
+})
+export class ThreeDCardComponent {
+
+}
