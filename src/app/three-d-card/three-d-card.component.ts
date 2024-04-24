fd6ce694c0ab0ae69c428de1bf2d6ee721ada269
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-04-24 20:07:38 +0200

    feat: reset the card perspective after blur

--- src/app/three-d-card/three-d-card.component.ts
+++ src/app/three-d-card/three-d-card.component.ts
@@ -8,15 +8,17 @@ import { Component } from "@angular/core";
   styleUrl: "./three-d-card.component.css",
 })
 export class ThreeDCardComponent {
+  private focusSubscription?: ReturnType<typeof setTimeout> | void;
 
   changePerspective(event: any) {
     const light = event.target.querySelector(".light") as HTMLElement;
-    const card = (event.target as HTMLElement);
+    const card = event.target as HTMLElement;
     const xFromCenter = event.layerX - card.offsetWidth / 2;
-    const yFromCenter = card.offsetHeight / 2 - event.layerY ;
+    const yFromCenter = card.offsetHeight / 2 - event.layerY;
 
     const lightAngle = this.calculateLightAngle(xFromCenter, yFromCenter);
 
+    card.style.transition = ""
     light.style.transform = `translate(${-xFromCenter}px, ${yFromCenter}px) rotate(${-lightAngle}deg)`;
     card.style.transform = `rotateX(${this.calculateCardAngle(card.offsetHeight, yFromCenter)}deg)  rotateY(${this.calculateCardAngle(card.offsetWidth, xFromCenter)}deg)`;
   }
@@ -26,6 +28,30 @@ export class ThreeDCardComponent {
   }
 
   calculateCardAngle(cardLenght: number, curPosition: number) {
-    return (curPosition / cardLenght * 14) 
+    return (curPosition / cardLenght) * 14;
+  }
+
+  resetPerspective(event: any) {
+    const resetDuration = 1500;
+    const card = (event.target as HTMLElement).getElementsByClassName("card")[0] as HTMLElement;
+    const light = event.target.querySelector(".light") as HTMLElement;
+
+    card.style.transition = `transform ${resetDuration}ms ease`;
+    light.style.transform = `translate(0px, 0px) rotate(0deg)`;
+    card.style.transform = `rotateY(0deg)  rotateX(0deg)`;
+
+    setTimeout(() => {
+      card.style.transition = "";
+    }, resetDuration);
+  }
+
+  focusCard() {
+    if (this.focusSubscription) clearTimeout(this.focusSubscription);
+  }
+
+  blurCard(event: any) {
+    this.focusSubscription = setTimeout(() => {
+      this.resetPerspective(event);
+    }, 2000);
   }
 }
