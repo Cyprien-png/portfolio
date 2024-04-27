423fe2c86dea0433d16659dd1ac80f6ed993d0ca
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-04-27 17:01:52 +0200

    feat: add skills section layout

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { ScrollButtonComponent } from '../../scroll-button/scroll-button.component';
 import { ThreeDCardComponent } from '../../three-d-card/three-d-card.component';
+import { MagneticCircleComponent } from '../../magnetic-circle/magnetic-circle.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavbarComponent, ScrollButtonComponent, ThreeDCardComponent],
+  imports: [NavbarComponent, ScrollButtonComponent, ThreeDCardComponent, MagneticCircleComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
