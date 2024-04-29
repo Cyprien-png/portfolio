b771878c62af0c084475e29fe4dcd0aaf8b97c9c
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-04-29 23:58:21 +0200

    refactor: replace the hard coded skills section by a dynamic one

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -4,14 +4,18 @@ import { ScrollButtonComponent } from '../../scroll-button/scroll-button.compone
 import { ThreeDCardComponent } from '../../three-d-card/three-d-card.component';
 import { MagneticCircleComponent } from '../../magnetic-circle/magnetic-circle.component';
 import { BorderedCircleComponent } from '../../bordered-circle/bordered-circle.component';
+import * as languageData from '../../../assets/data/languages.json';
+import * as frameworkData from '../../../assets/data/frameworks.json';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [NavbarComponent, ScrollButtonComponent, ThreeDCardComponent, MagneticCircleComponent, BorderedCircleComponent],
+  imports: [NavbarComponent, ScrollButtonComponent, ThreeDCardComponent, MagneticCircleComponent, BorderedCircleComponent, NgFor],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
-
+  languages: any = (languageData as any).default;
+  frameworks: any = (frameworkData as any).default;
 }
