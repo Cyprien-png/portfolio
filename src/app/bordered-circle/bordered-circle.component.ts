72ee2fd893fd3745b60686ede95d5a1447debb3e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-04-29 22:47:04 +0200

    feat: add icon and link params on bordered circles

--- src/app/bordered-circle/bordered-circle.component.ts
+++ src/app/bordered-circle/bordered-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bordered-circle',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './bordered-circle.component.css'
 })
 export class BorderedCircleComponent {
+  @Input() imgsrc = "";
+  @Input() linkhref = "";
 
 }
