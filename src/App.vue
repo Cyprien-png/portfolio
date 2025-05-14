81fff2ef530ded13e4cf782e58f4e140851b88a5
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-05-14 22:48:00 +0200

    feat: add a tmp placeholder 'welcome' message

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,17 @@ import Background from './components/Background.vue';
 </script>
 
 <template>
-  <section class="h-screen w-screen overflow-hidden">
-    <Background class="h-screen w-screen absolute inset-0 overflow-hidden"/>
+  <section
+    class="h-screen w-screen relative text-white font-custom-code flex justify-center items-center overflow-hidden">
+    <Background class="h-screen w-screen absolute inset-0 -z-10" />
+    <div class="flex flex-col p-8 text-justify">
+      <h1 class="text-3xl uppercase">H3ll0 th3r3!</h1>
+      <span>
+        I'm currently developing this portfolio
+        <a href="https://github.com/Cyprien-png/portfolio" class="underline">(feel free to read the code).</a>
+      </span>
+      <span>Check back soon for updates!</span>
+      <span class="pt-4">Cyprien Jaquier.</span>
+    </div>
   </section>
 </template>
-
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -3,7 +3,7 @@ import Particles from './Particles.vue';
 </script>
 
 <template>
-  <div class="h-full w-full relative bg-custom-blue-dark overflow-hidden">
+  <div class="bg-custom-blue-dark overflow-hidden">
     <Particles class="blur-[7rem]"/>
     <div class="overlay bg-noise"></div>
   </div>
