8c13f03d5033a5eb5d670884b45ec267313612a3
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-05-14 22:12:47 +0200

    feat: add animated background

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import Particles from './components/Particles.vue';
+
 </script>
 
 <template>
-  <div class="bg-custom-blue-dark h-screen relative">
+  <div class="bg-custom-blue-dark h-screen relative overflow-hidden">
+    <Particles class="blur-[7rem]"/>
+    
+    <div id="particles-js"></div>
     <div class="overlay bg-noise"></div>
   </div>
 </template>
+
---
+++ src/components/Particles.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from 'vue'
+import particlesConfig from '../assets/particles.config.json';
+
+onMounted(() => {
+  const particlesScript = document.createElement('script')
+  particlesScript.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'
+  particlesScript.onload = initializeParticles
+  document.head.appendChild(particlesScript)
+
+  const statsScript = document.createElement('script')
+  statsScript.src = 'https://threejs.org/examples/js/libs/stats.min.js'
+  document.head.appendChild(statsScript)
+})
+
+function initializeParticles() {
+  particlesJS("particles-js", particlesConfig);
+  requestAnimationFrame(update);
+}
+</script>
+
+<template>
+  <div id="particles-js"></div>
+</template>
