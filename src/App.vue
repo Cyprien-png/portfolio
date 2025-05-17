65424467ee48c729f4c891ab218127f1b1ad8bd9
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-05-17 22:37:26 +0200

    feat: add a custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Background from './components/Background.vue';
 import Navbar from './components/Navbar.vue';
 import Cursor from './components/Cursor.vue';
+import Scrollbar from './components/Scrollbar.vue';
 </script>
 
 <template>
@@ -11,14 +12,17 @@ import Cursor from './components/Cursor.vue';
     </div>
     <Background class="h-dvh w-dvw absolute inset-0 -z-10" />
 
-    <div class="w-screen h-screen flex flex-col border-x-[1rem] border-white overflow-auto">
-      
-      <Navbar class="border-t-[1rem] border-white"/>
+    <div class="w-screen h-screen flex flex-col border-x-[1rem] border-white overflow-auto no-scrollbar">
+      <div id="scrollable-screen" class="flex-1 flex flex-col">
+        <Scrollbar />
+        <Navbar class="border-t-[1rem] border-white" />
 
-      <div class="px-24 flex-1 border-b-[1rem] border-white">
-        <h1 class="flex flex-col uppercase font-custom-anton font-extrabold text-center text-[8dvw]">
-          Full Stack Developer
-        </h1>
+        <div class="px-24 flex-1 border-b-[1rem] border-white">
+          <h1 class="flex flex-col uppercase font-custom-anton font-extrabold text-center text-[8dvw]">
+            Full Stack Developer
+          </h1>
+
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/Scrollbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const thumbY = ref(0);
+const thumbH = ref(0);
+const isScollable = ref(false);
+
+onMounted(() => {
+    const body = document.querySelector('body');
+    const scrollableScreen = document.getElementById('scrollable-screen');
+    const scrollTrack = document.getElementById('scroll-track');
+    const scrollContainer = scrollableScreen.parentElement;
+    
+    const updateScrollbar = () => {
+        isScollable.value = scrollableScreen.clientHeight > scrollContainer.clientHeight;
+        const coeff = scrollableScreen.clientHeight / scrollTrack.clientHeight
+        thumbH.value = body.clientHeight / coeff;
+        thumbY.value = scrollContainer.scrollTop / coeff;
+    }
+
+    updateScrollbar();
+    scrollContainer.addEventListener('scroll', updateScrollbar);
+})
+</script>
+
+<template>
+    <div class="absolute top-0 right-0 w-4 h-full py-4">
+        <div id="scroll-track" class="w-full h-full relative flex justify-center">
+            <div v-if="isScollable" :data-scroll-thumb-h="thumbH" :data-scroll-thumb-y="thumbY"
+                :style="`height: ${thumbH}px; top: ${thumbY}px`" class="w-2/3 h-12 bg-custom-blue-dark absolute top-0">
+            </div>
+        </div>
+    </div>
+</template>
