334f087535c6a6ca28181656dd170e7b59346fbe
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-06-23 22:31:34 +0200

    refactor: enhance the scrollbar by adding the 'scrolled percent' info in the thumb

--- src/components/Scrollbar.vue
+++ src/components/Scrollbar.vue
@@ -3,8 +3,10 @@ import { onMounted, onUnmounted, ref } from 'vue';
 
 const thumbY = ref(0);
 const isScollable = ref(false);
+const coeff = ref(0);
 
 onMounted(() => {
+    const borderWithSum = 32;
     const scrollableScreen = document.getElementById('scrollable-screen');
     const scrollTrack = document.getElementById('scroll-track');
     const scrollContainer = scrollableScreen.parentElement;
@@ -20,8 +22,8 @@ onMounted(() => {
         const maxScrollTrack = scrollTrack.clientHeight - scrollThumb.clientHeight + 2;
 
         // calulate the coefficient of scrolled pixels
-        const coeff = scrollContainer.scrollTop / maxScrollScreen;
-        thumbY.value = Math.round(maxScrollTrack * coeff);
+        coeff.value = scrollContainer.scrollTop / (maxScrollScreen + borderWithSum);
+        thumbY.value = Math.round(maxScrollTrack * coeff.value);
     }
 
     updateScrollbar();
@@ -40,7 +42,10 @@ onUnmounted(() => {
     <div class="absolute top-0 right-0 w-4 h-full py-4">
         <div id="scroll-track" class="w-full h-full relative flex justify-center">
             <div id="scroll-thumb" v-if="isScollable" :data-scroll-thumb-y="thumbY"
-                :style="`top: ${thumbY - 1}px`" class="w-5/8 bg-custom-blue-dark absolute top-0 h-4 rounded-xs">
+                :style="`top: ${thumbY - 1}px`" class="w-5/8 bg-custom-blue-dark absolute top-0 h-8 rounded-xs flex items-center justify-center">
+                <span class="text-xs text-white rotate-270 text-left items-center justify-center">
+                    {{Math.round(coeff * 100)}}%
+                </span>
             </div>
         </div>
     </div>
