dc40877e4ba72a7eff728b7e8178d5de2b8a59e4
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2025-10-08 18:58:29 +0200

    feat: add hard static home page

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+    <section class="w-dvw h-dvh overflow-hidden p-10">
+        <div class="h-full w-full p-4 bg-fit bg-cover bg-position-[center_40%] rounded-3xl bg-[url('/placeholder.png')] flex flex-col justify-center items-center text-center">
+            <p class="text-[#fff] font-rubik uppercase text-[10dvw] leading-[7.33dvw] xl:text-[15dvh] xl:leading-[11dvh] tracking-tighter">
+                Here<br/>
+                to <span class="text-red-500">build</span><br/>
+                thefuture<br/>
+                one line<br/>
+                at a time.
+            </p>
+        </div>
+    </section>
+</template>
+
+<style scoped></style>
