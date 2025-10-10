d4d10517d02aaa033727e34b17481a90ed3e4332
Cyprien-png <cyprien04@hotmail.com>
2025-10-11 00:23:40 +0200

    style: replace the 'home' title from navbar by a dynamic arrow

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue'
 import { useScrollContext } from '@/composables/useScrollContext'
+import ArrowIcon from '@/icons/ArrowIcon.vue'
 
 const { containerRef, sections, contentRef } = useScrollContext()
 
@@ -52,16 +53,16 @@ const handleScroll = () => {
   const isAtBottom =
     Math.ceil(containerRef.value.scrollTop + containerRef.value.clientHeight) >= containerRef.value.scrollHeight
 
- 
-    sectionsEls.forEach((section) => {
-      const rect = section.getBoundingClientRect()
-      const containerRect = containerRef.value.getBoundingClientRect()
-      const offsetTop = rect.top - containerRect.top
-      if (offsetTop <= 0 && current.getBoundingClientRect().top < section.getBoundingClientRect().top) {
-        current = section
-      }
-    })
-  
+
+  sectionsEls.forEach((section) => {
+    const rect = section.getBoundingClientRect()
+    const containerRect = containerRef.value.getBoundingClientRect()
+    const offsetTop = rect.top - containerRect.top
+    if (offsetTop <= 0 && current.getBoundingClientRect().top < section.getBoundingClientRect().top) {
+      current = section
+    }
+  })
+
   const matchedSection = sections.value.find((s) => s.id === current.id)
   if (matchedSection) currentSection.value = matchedSection.id
   updateNav()
@@ -83,31 +84,22 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <nav
-    ref="nav"
-    class="fixed flex gap-1 rounded-full top-[4dvw] left-1/2 -translate-x-1/2 z-50 bg-[#0004] outline-2 outline-[#aaa8] backdrop-blur-sm"
-  >
-    <div
-      ref="indicator"
-      id="indicator"
-      class="bg-white absolute inset-0 rounded-full h-full"
-    ></div>
+  <nav ref="nav"
+    class="fixed flex gap-1 rounded-full top-[4dvw] left-1/2 -translate-x-1/2 z-50 bg-[#0004] outline-2 outline-[#aaa8] backdrop-blur-sm">
+    <div ref="indicator" id="indicator" class="bg-white absolute inset-0 rounded-full h-full"></div>
 
-    <a
-      v-for="section in sections"
-      :key="section.id"
+    <a v-for="section in sections" :key="section.id"
       class="nav-element cursor-pointer px-4 py-2 relative text-white mix-blend-difference first-letter:capitalize"
-      :data-section="section.id"
-      @click="selectSection(section.id)"
-    >
-      {{ section.id.replace('-', ' ') }}
+      :data-section="section.id" @click="selectSection(section.id)">
+      <ArrowIcon v-if="section.id == 'home'" class="h-5 w-4 translate-y-0.5 transition-all" :class="currentSection=='home'? 'rotate-90':'-rotate-90'" />
+      <span v-else>{{ section.id.replace('-', ' ') }}</span>
     </a>
   </nav>
 </template>
 
 <style scoped>
 #indicator {
-   transition:
+  transition:
     transform .2s ease-out,
     width .2s ease-out;
 }
