6cc8e960fbb9aa05d9f9abfc5a87433e00262359
Cyprien-png <cyprien04@hotmail.com>
2025-10-12 17:16:58 +0200

    fix: re-center the imageAsCursor

--- src/components/ImageAsCursor.vue
+++ src/components/ImageAsCursor.vue
@@ -5,30 +5,39 @@ import { useImageAsCursor } from '@/composables/useImageAsCursor'
 const { imageUrl, isOpen } = useImageAsCursor()
 
 // reactive cursor position
-const x = ref(0)
-const y = ref(0)
+const x = ref(window.innerWidth / 2)
+const y = ref(window.innerHeight / 2)
 
 // smooth interpolation
-let targetX = 0
-let targetY = 0
+let targetX = x.value
+let targetY = y.value
 let rafId = null
 
 const updatePosition = (e) => {
-  if (!isOpen.value) return;
   targetX = e.clientX
   targetY = e.clientY
 }
 
+const shouldMove = (position, targetPosition) => {
+  return Math.round(position.value * 100) / 100 != targetPosition && !!isOpen.value
+}
+
 const animate = () => {
   // smooth easing
-  x.value += (targetX - x.value) * 0.2
-  y.value += (targetY - y.value) * 0.2
+  if (shouldMove(x.value, targetX)) {
+    x.value += (targetX - x.value) * 0.2
+  }
+
+  if (shouldMove(y.value, targetY)) {
+    y.value += (targetY - y.value) * 0.2
+  }
 
   rafId = requestAnimationFrame(animate)
 }
 
 onMounted(() => {
   window.addEventListener('mousemove', updatePosition)
+
   animate()
 })
 
@@ -41,9 +50,7 @@ onUnmounted(() => {
 <template>
   <div
     class="aspect-[16/9] absolute z-40 -translate-x-1/2 -translate-y-1/2 pointer-events-none rounded-full overflow-hidden transition-all duration-75"
-    :class="isOpen ? 'w-1/6' : 'w-0'"
-    :style="{ left: `${x}px`, top: `${y}px` }"
-  >
+    :class="isOpen ? 'w-1/6' : 'w-0'" :style="`left: ${x}px; top: ${y}px`">
     <img :src="imageUrl" alt="" class="w-full h-full object-cover" />
   </div>
 </template>
