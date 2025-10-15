ad4e24d5c6159396729ba7cfea4c7337807f3b80
Cyprien-png <cyprien04@hotmail.com>
2025-10-15 20:53:17 +0200

    style: realign the texts in the awards section

--- src/views/Awards.vue
+++ src/views/Awards.vue
@@ -6,10 +6,12 @@ import awards from '@/data/awards.json'
 <template>
      <FramedMainSection id="awards" class="h-dvh">
         <div class="h-full w-full rounded-4xl bg-yellow-950 text-white flex justify-center items-end text-center p-4">
-            <div v-for="award in awards" class="h-1/2 flex-1 p-4 flex flex-col items-start cursor-default">
-                <span class="font-rubik">*{{award.date}}</span>
-                <h1 class="font-rubik text-2xl">{{award.title}}</h1>
-                <p class="text-justify">{{award.description}}</p>
+            <div class="flex">
+                <div v-for="award in awards" class="h-fit flex-1 p-4 flex flex-col items-start cursor-default">
+                    <span class="font-rubik">*{{award.date}}</span>
+                    <h1 class="font-rubik text-2xl">{{award.title}}</h1>
+                    <p class="text-justify">{{award.description}}</p>
+                </div>
             </div>
         </div>
     </FramedMainSection>
