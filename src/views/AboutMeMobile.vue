6c46c649c8f86e7f27eb410c0cb04efdd19697d2
Cyprien-png <cyprien04@hotmail.com>
2025-10-18 21:31:17 +0200

    feat: rework the mobile version of aboutme section in order to fit the futur images

--- src/views/AboutMeMobile.vue
+++ src/views/AboutMeMobile.vue
@@ -5,12 +5,14 @@ import stories from '@/data/about-me.json'
 
 <template>
     <FramedMainSection id="about-me" class="min-h-[100dvh] flex relative">
-        <div class="h-full w-full rounded-4xl bg-yellow-950 text-white flex justify-center items-end text-center p-4">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div class="h-full w-full rounded-4xl bg-gray-800 text-white flex justify-center items-end text-center p-4">
+            <div class="grid grid-cols-1 md:grid-cols-3">
 
                 <div v-for="(story, i) in stories" :key="i"
                     class="h-fit flex-1 p-4 flex flex-col items-start cursor-default text-left gap-2">
-                    <img :src="story.image" class="rounded-2xl object-contain aspect-video md:w-full" />
+                    <div class="rounded-2xl object-contain aspect-video w-full overflow-hidden">
+                        <img :src="story.image" class="h-full w-full" />
+                    </div>
                     <p class="text-justify">{{ story.description }}</p>
                 </div>
             </div>
