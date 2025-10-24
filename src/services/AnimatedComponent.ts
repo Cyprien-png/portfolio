fc338458e136eea8e51b813fd25e7f344f8d3d54
Cyprien-png <cyprien04@hotmail.com>
2025-10-24 02:02:37 +0200

    feat: add a new AnimatedComponent class that will be used as a

---
+++ src/services/AnimatedComponent.ts
@@ -0,0 +1,59 @@
+/**
+ * A vue component adapter used to delegate the animations management to optimized services
+ * @property {symbol} id - Unique ID of the component instance
+ * @property {boolean} areAnimationsEnabled - Status of animation capability
+ * @property {HTMLElement} domSection - The section / container that must be visible to enable the animation
+ * @property {void} enableAnimations - The trigger to enable animations
+ * @property {void} disableAnimations - The trigger to disable animations
+ * @property {void} prepareForAnimations - The computation of animation data
+ * @property {void} tick - The animation "apply" method
+ */
+export class AnimatedComponent {
+    public id: symbol;
+    public areAnimationsEnabled: boolean;
+    public domSection: HTMLElement;
+    public enableAnimations: () => void;
+    public disableAnimations: () => void;
+    public prepareForAnimations: () => void;
+    public tick: () => void;
+
+    constructor(el: HTMLElement) {
+        this.domSection = el;
+    }
+
+    /**
+     * Check if the animation is on a visible section
+     * @returns True if the animation is visible
+     */
+    private isVisibleOnScreen(): boolean {
+        const isAboveScreen = this.domSection.getBoundingClientRect().bottom < 0;
+        const isBelowScreen = this.domSection.getBoundingClientRect().top - window.innerHeight > 0
+        return !isAboveScreen && !isBelowScreen
+    }
+
+    /**
+     * Enable / Disable the animations based on current status
+     */
+    private toggleAnimationStatus(): void {
+        this.areAnimationsEnabled ? this.disableAnimations() : this.enableAnimations();
+        this.areAnimationsEnabled = !this.areAnimationsEnabled;
+    }
+
+    /**
+     * Compute and run the animation
+     */
+    public animate(): void {
+        // Ensure that the animation is visible
+        if (!this.isVisibleOnScreen()) {
+            if (this.areAnimationsEnabled) this.toggleAnimationStatus();
+            return;
+        }
+
+        // Compute all the data for the animation tick
+        if (!this.areAnimationsEnabled) this.toggleAnimationStatus();
+        this.prepareForAnimations();
+
+        // Delegate the animation sync to the browser
+        requestAnimationFrame(this.tick);
+    }
+}
