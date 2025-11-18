f561323385640a820618813dba09b70b191fdaba
Cyprien-png <cyprien04@hotmail.com>
2025-11-18 17:09:09 +0100

    style: change color of the testimonials stars

--- src/components/TestimonialWriterCard.vue
+++ src/components/TestimonialWriterCard.vue
@@ -26,7 +26,7 @@ const props = defineProps({
         <img :src="writerImage" alt="" class="h-12 w-12 rounded-full">
         <div class="pl-2">
             <div class="flex">
-                <starIcon v-for="i in 5" :class="i <= starsCount ? (isDarkMode?'text-yellow-500':'text-black') : (isDarkMode?'text-transparent':'text-white')" />
+                <starIcon v-for="i in 5" :class="i <= starsCount ? 'text-yellow-500' : (isDarkMode?'text-transparent':'text-white')" />
             </div>
             <span class="italic text-sm" :class="isDarkMode? 'text-neutral-100':'text-neutral-600'">{{ writerPosition }}</span>
         </div>
