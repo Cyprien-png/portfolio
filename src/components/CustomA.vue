9f3a514df4f3401f296f1963f9889ad0e480e107
Cyprien-png <cyprien04@hotmail.com>
2025-11-22 14:10:21 +0100

    feat: add a custom component for the A tag

---
+++ src/components/CustomA.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    text: { type: String, required: true },
+    href: { type: String, default: "#" },
+    target: { type: String, default: "" }
+});
+
+const letters = computed(() => props.text.split(""));
+</script>
+
+<template>
+    <a :href="href" :target="target" class="relative group flex overflow-hidden">
+        <span class="flex">
+            <span v-for="(l, i) in letters" :key="i"
+                class="inline-block transition-all duration-200 group-hover:-translate-y-full"
+                :style="{ '--i': i, 'transition-delay': `calc(var(--i) * 30ms)` }">
+                {{ l }}
+            </span>
+        </span>
+
+        <span class="absolute top-full left-0 flex">
+            <span v-for="(l, i) in letters" :key="'bot-' + i"
+                class="inline-block text-red-custom transition-all duration-200 group-hover:-translate-y-full"
+                :style="{ '--i': i, 'transition-delay': `calc(var(--i) * 30ms)` }">
+                {{ l }}
+            </span>
+        </span>
+    </a>
+</template>
