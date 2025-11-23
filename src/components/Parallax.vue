4aa0b0ff2d9f7ef7be1e012a4cc9db0248029a92
Cyprien-png <cyprien04@hotmail.com>
2025-11-23 17:46:06 +0100

    feat: add auto animations of the parallax on mobile view

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,21 +1,29 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { AnimatedComponent } from '@/services/AnimatedComponent'
-import { useScrollContext } from '@/composables/useScrollContext';
 import { useCursorContext } from '@/composables/useCursorContext';
 
-const { containerRef} = useScrollContext();
 const { getPositions } = useCursorContext();
 
 const rootRef = ref();
 const component = ref();
 const parallaxItems = ref([]);
+const autoAnimDegree = ref(0);
 
 const updatePosition = () => {
+  let cur = getPositions();
+
+  if (window.innerWidth < 768) {
+    autoAnimDegree.value = (autoAnimDegree.value + 0.005)%360;
+    const hyp = 500;
+
+    cur.x = hyp * Math.cos(autoAnimDegree.value * 1.5)
+    cur.y = hyp * Math.sin(autoAnimDegree.value)    
+  };
+
   const rootRect = rootRef.value.getBoundingClientRect();
   const relativeCursorX = rootRect.width / 2 + rootRect.left;
   const relativeCursorY = rootRect.height / 2 + rootRect.top;
-  const cur = getPositions();
 
   parallaxItems.value.forEach(el => {
     const multiplicator = el.dataset.parallaxValue;
@@ -30,8 +38,7 @@ onMounted(() => {
   parallaxItems.value = Array.from(rootRef.value.getElementsByClassName("parallax")) ;
   component.value = new AnimatedComponent(rootRef.value);
   component.value.tick = updatePosition;
-  component.value.addAnimationTrigger(window, "mousemove");
-  component.value.addAnimationTrigger(containerRef.value, "scroll");
+  component.value.autoAnimate();
 })
 </script>
 
